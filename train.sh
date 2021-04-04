507c36d60139e8566dc4283d7ab3eb547845b80a
Yazhou XING <yzxing87@outlook.com>
2021-04-04 18:44:46 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
+python train.py --task=debug \
+                --data_path="./data/" \
+                --gamma \
+                --aug \
+                --camera="NIKON_D700" \
+                --out_path="./exps/" \
+                # --debug_mode
+
 # python train.py --task=debug2 \
 #                 --data_path="./data/" \
 #                 --gamma \
 #                 --aug \
-#                 --camera="NIKON_D700" \
+#                 --camera="Canon_EOS_5D" \
 #                 --out_path="./exps/" \
-#                 # --debug_mode
-
-python train.py --task=debug2_canon \
-                --data_path="./data/" \
-                --gamma \
-                --aug \
-                --camera="Canon_EOS_5D" \
-                --out_path="./exps/" \
-                --debug_mode
+#                 --debug_mode
