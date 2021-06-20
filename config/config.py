5c0ada42b833c9bbb3b8f152f53696ce6e304e50
Yazhou Xing <yzxing87@outlook.com>
2021-06-20 19:39:11 +0800

    fix typos

--- config/config.py
+++ config/config.py
@@ -14,7 +14,7 @@ def get_arguments():
     parser.add_argument("--batch_size", type=int, default=BATCH_SIZE, help="Batch size for training. ")       
     parser.add_argument("--debug_mode", dest='debug_mode', action='store_true',  help="If debug mode, load less data.")    
     parser.add_argument("--gamma", dest='gamma', action='store_true', help="Use gamma compression for raw data.")     
-    parser.add_argument("--camera", type=str, default="NIKON D700", choices=["NIKON_D700", "Canon_EOS_5D"], help="Choose which camera to use. ")    
+    parser.add_argument("--camera", type=str, default="NIKON_D700", choices=["NIKON_D700", "Canon_EOS_5D"], help="Choose which camera to use. ")    
     parser.add_argument("--rgb_weight", type=float, default=1, help="Weight for rgb loss. ")                 
     
     
